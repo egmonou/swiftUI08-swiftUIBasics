7284224b21fb8f8aca87be13a9a9749f28eeed88
Nour <mohamednour25@gmail.com>
2024-08-30 18:52:42 +0300

    Initial Commit

---
+++ swiftUIBasics/swiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIBasicsApp.swift
+//  swiftUIBasics
+//
+//  Created by administrator on 30/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
