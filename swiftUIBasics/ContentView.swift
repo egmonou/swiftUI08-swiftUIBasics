db7c3cbe0b56735f18b38e2c848d87a864887a48
Nour <mohamednour25@gmail.com>
2024-09-01 08:59:13 +0300

    90

--- swiftUIBasics/ContentView.swift
+++ swiftUIBasics/ContentView.swift
@@ -1,23 +1,73 @@
-//
-//  ContentView.swift
-//  swiftUIBasics
-//
-//  Created by administrator on 30/08/2024.
-//
+
+//SwiftUI Basics
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        //        VStack(spacing: 0) {
+        //            Text("SwiftUI Basics1")
+        //                .font(.largeTitle)
+        //                .fontWeight(.semibold)
+        //            .foregroundStyle(.red)
+        //
+        //            Text("SwiftUI Basics2")
+        //                .font(.largeTitle)
+        //                .fontWeight(.semibold)
+        //            .foregroundStyle(.green)
+        //
+        //            Text("SwiftUI Basics3")
+        //                .font(.largeTitle)
+        //                .fontWeight(.semibold)
+        //            .foregroundStyle(.blue)
+        //        }
+        
+        
+        
+        //        ZStack {
+        //            //Background Color
+        //            Color
+        //                .red
+        //                .opacity(0.1)
+        //                .ignoresSafeArea()
+        //            //Text
+        //            Text("SwiftUI Basics1")
+        //                .font(.largeTitle)
+        //                .fontWeight(.semibold)
+        //            .foregroundStyle(.blue)
+        //        }
+        
+        
+        
+   //     ZStack {
+            //            //Background Color
+            //            Color
+            //                .red
+            //                .opacity(0.1)
+            //                .ignoresSafeArea()
+            //            //Text
+            //            Text("SwiftUI Basics1")
+            //                .font(.largeTitle)
+            //                .fontWeight(.semibold)
+            //            .foregroundStyle(.blue)
+            //        }
+            
+            
+            ZStack {
+                //Background Color
+               LinearGradient(colors: [.red, .purple, .green, .pink],
+                startPoint: .topLeading, endPoint: .bottomTrailing)
+               .opacity(0.5)
+               .ignoresSafeArea()
+                //Text
+                Text("SwiftUI Basics1")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.blue)
+            }
     }
 }
+
 
 #Preview {
     ContentView()
