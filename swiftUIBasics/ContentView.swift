4d454df3bcfe05934735e4dbd104e8d5a2d20f4f
Nour <mohamednour25@gmail.com>
2024-09-02 12:44:27 +0300

    91

--- swiftUIBasics/ContentView.swift
+++ swiftUIBasics/ContentView.swift
@@ -4,40 +4,60 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    let colorList1: [Color] =  [.black, .white, .blue]
+    let colorList2: [Color] = [.red, .purple, .green, .pink]
+    let rotationAngel: CGFloat = 360
     @State private var screenTapped: Bool = false
-    @State private var offsetY: CGFloat = -150
-
+    
     var body: some View {
-            let colorList1: [Color] =  [.black, .white, .blue]
-            let colorList2: [Color] = [.red, .purple, .green, .pink]
+
             ZStack {
                 //Background Color
                 LinearGradient(colors: screenTapped ? colorList1 : colorList2,
                 startPoint: .topLeading, endPoint: .bottomTrailing)
                .opacity(0.8)
                .ignoresSafeArea()
-                //Text
-                Text("SwiftUI Basics1")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
-                    .foregroundStyle(.blue)
-                    .offset(y: offsetY)
+                
+                VStack(spacing: 0) {
+                    Spacer()
+                    //Text1
+                    Text(screenTapped ? "The Basics of SwiftUI" : "To know what yo know and what do now know, that is true knoledge.")
+                        .font(screenTapped ? .largeTitle: .title)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.red)
+                        .multilineTextAlignment(.center)
+                    
+                        .rotation3DEffect(
+                            .degrees(screenTapped ? .zero: rotationAngel),axis: (x: 1, y: 1, z: 1)
+                        )
+                        .padding(.horizontal)
+                    Spacer()
+                    //Text2
+                    Text("Antheor line of Text")
+                        .font(screenTapped ? .largeTitle: .title)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(screenTapped ? .blue : .white)
+                        .multilineTextAlignment(.center)
+                    
+                        .rotation3DEffect(
+                            .degrees(screenTapped ? .zero: rotationAngel),axis: (x: 1, y: 1, z: 1)
+                        )
+                        .padding(.horizontal)
+                    Spacer()
+                }
+                
             }
+            //change screen color if tapped
             .onTapGesture {
-                //change screen color if tapped
-                screenTapped.toggle()
-                withAnimation(.easeIn(duration: 1)) {
-                    if screenTapped {
-                        offsetY = -1000
-                    }else {
-                        offsetY = 0
-                    }
+                withAnimation {
+                    screenTapped.toggle()
+                }
+
                 }
                 
             }
     }
-}
+
 
 
 struct ContentView_Previews: PreviewProvider {
